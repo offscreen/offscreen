a56548459c4ae81b6398bbe0f712256e884e4d75
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-01 00:27:48 +0700

    manage

---
+++ resource/script/canonical.js
@@ -0,0 +1,4 @@
+var link = document.createElement('link');
+link.setAttribute('rel', 'canonical');
+link.setAttribute('href', location.protocol + '//' + location.host + location.pathname);
+document.head.appendChild(link);
