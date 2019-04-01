e7391d6650701bf59224450ef23592c9f50886fa
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-01 18:11:38 +0700

    Update canonical.js

--- resource/script/canonical.js
+++ resource/script/canonical.js
@@ -1,4 +1,4 @@
 var link = document.createElement('link');
 link.setAttribute('rel', 'canonical');
-link.setAttribute('href', location.protocol + '//' + location.host + location.pathname);
+link.setAttribute('href', location.protocol + '://' + location.host + location.pathname);
 document.head.appendChild(link);
