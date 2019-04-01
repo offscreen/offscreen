6deb49c2aaaf62e079cca9957bb077e0fc9db48e
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-01 18:27:54 +0700

    manage

--- resource/script/canonical.js
+++
@@ -1,4 +0,0 @@
-var canonical = document.createElement('link');
-canonical.setAttribute('rel', 'canonical');
-canonical.setAttribute('href', location.protocol + '://' + location.host + location.pathname);
-document.head.appendChild(canonical);
