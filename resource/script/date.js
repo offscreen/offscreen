d01b62996e0d326baf75976b141ab496ba6d831c
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-01 18:16:06 +0700

    manage

---
+++ resource/script/date.js
@@ -0,0 +1,1 @@
+(function(){var dn=new Date(),da=dn.getDate(),mo=dn.getMonth(),ye=dn.getFullYear(),elm=document.getElementById("now_d");function month(a){var mo=["January","February","March","April","May","June","July","August","September","October","November","December"];return mo[a];}dn=da+" "+month(mo)+" "+ye;elm.innerHTML=dn;}());
