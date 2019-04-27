dbf0aeb0a66d03db4c62aec6bb7eed6d03018232
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-27 18:09:51 +0700

    Update url-form-field.js

--- resource/script/url-form-field.js
+++ resource/script/url-form-field.js
@@ -1,20 +1,20 @@
-  /* from https://aaronparecki.com/2018/06/03/4/url-form-field */
-  /* add http:// to URL fields on blur or when enter is pressed */
-  document.addEventListener('DOMContentLoaded', function() {
+/* from https://aaronparecki.com/2018/06/03/4/url-form-field */
+/* add http:// to URL fields on blur or when enter is pressed */
+document.addEventListener('DOMContentLoaded', function() {
     function addDefaultScheme(target) {
-      if(target.value.match(/^(?!https?:).+\..+/)) {
-        target.value = "https://"+target.value;
-      }
+        if (target.value.match(/^(?!https?:).+\..+/)) {
+            target.value = "https://" + target.value;
+        }
     }
     var elements = document.querySelectorAll("input[type=url]");
-    Array.prototype.forEach.call(elements, function(el, i){
-      el.addEventListener("blur", function(e){
-        addDefaultScheme(e.target);
-      });
-      el.addEventListener("keydown", function(e){
-        if(e.keyCode == 13) {
-          addDefaultScheme(e.target);
-        }
-      });
+    Array.prototype.forEach.call(elements, function(el, i) {
+        el.addEventListener("blur", function(e) {
+            addDefaultScheme(e.target);
+        });
+        el.addEventListener("keydown", function(e) {
+            if (e.keyCode == 13) {
+                addDefaultScheme(e.target);
+            }
+        });
     });
-  });
+});
