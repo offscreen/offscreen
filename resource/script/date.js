11694b102b90d2da023339fc29707a39e017609a
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-29 12:39:53 +0700

    m

--- resource/script/date.js
+++ resource/script/date.js
@@ -1,1 +1,22 @@
-(function(){var dn=new Date(),da=dn.getDate(),mo=dn.getMonth(),ye=dn.getFullYear(),elm=document.getElementById("now_d");function month(a){var mo=["January","February","March","April","May","June","July","August","September","October","November","December"];return mo[a];}dn=da+" "+month(mo)+" "+ye;elm.innerHTML=dn;}());
+(function() {
+    var dn = new Date(),
+        da = dn.getDate(),
+        mo = dn.getMonth(),
+        ye = dn.getFullYear(),
+        elm = document.getElementById("now_d");
+
+    function month(a) {
+        var mo = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        return mo[a];
+    }
+    dn = da + " " + month(mo) + " " + ye;
+    elm.innerHTML = dn;
+}());
+
+(function() {
+    var clock = document.getElementById('clock');
+    setInterval(function() {
+        var time = new Date().toString().split(' ')[4];
+        clock.innerHTML = time;
+    }, 13);
+})();
