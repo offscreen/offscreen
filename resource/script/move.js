f62d97c696822819ce0d8508d266ad34be010c87
offscreen <49018593+offscreen@users.noreply.github.com>
2019-04-29 07:15:06 +0700

    Update move.js

--- resource/script/move.js
+++ resource/script/move.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $('#move-up').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 'slow');
+        }, 'fast');
         return false;
     });
     $('#move-down').click(function() {
         $('html, body').animate({
             scrollTop: $(document).height()
-        }, 'slow');
+        }, 'fast');
         return false;
     });
 });
