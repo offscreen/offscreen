36bb07775518fb5b00a8570c59979d4220cf484c
offscreen <49018593+offscreen@users.noreply.github.com>
2019-05-12 07:38:57 +0700

    m

--- resource/script/window-location.js
+++ resource/script/window-location.js
@@ -9,5 +9,4 @@ window.onload = function() {
     document.getElementById('port').value = window.location.port;
     document.getElementById('protocol').value = window.location.protocol;
     document.getElementById('search').value = window.location.search;
-    document.getElementById('source').value = 'view-source:' + window.location.href;
 }
